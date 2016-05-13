20ea2445395fd33da36763c3df6dc77c4bc71234
randbrown <brown.randy@gmail.com>
2016-05-12 23:37:22 -0400

    Removed all the unused starter template stuff since this only needs one view.

--- FRCmap/www/js/app.js
+++ FRCmap/www/js/app.js
@@ -22,25 +22,4 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 })
-
-.config(function($stateProvider, $urlRouterProvider) {  
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-  // Each tab has its own nav history stack:
-
-  .state('dash', {
-    url: '/dash',
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/dash');
-
-});
+;
